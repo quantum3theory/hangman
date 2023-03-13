dcefe13c4a62d2bef455b1ee6d4e7d67fc51e43d
quantum3theory <ciprian.pantea@yahoo.com>
2023-03-13 21:56:05 +0000

    adds corrections to reflect AiCore's setup

--- milestone_5.py
+++ milestone_5.py
@@ -49,19 +49,19 @@ class Hangman:
                 self.list_of_guesses.append(guess)
                 break
             
-def play_game(word_list):
-    word_list = [word_list]
-    num_lives = 5
-    game = Hangman(word_list, num_lives)
-    while True:
-        if game.num_lives == 0:
-            print('You lost!')
-            break
-        elif game.num_letters > 0:
-            game.ask_for_input()
-        elif num_lives != 0 and game.num_letters <= 0:
-            print('Congratulation. You won the game!')
-            break
+    def play_game(word_list):
+        word_list = [word_list]
+        num_lives = 5
+        game = Hangman(word_list, num_lives)
+        while True:
+            if game.num_lives == 0:
+                print('You lost!')
+                break
+            elif game.num_letters > 0:
+                game.ask_for_input()
+            elif num_lives != 0 and game.num_letters <= 0:
+                print('Congratulation. You won the game!')
+                break
             
             
-game_1 = play_game('banana')
+game_1 = Hangman.play_game('banana')
