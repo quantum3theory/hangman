9f9935cd0ddcdbef36b72ba1eac92595ec91179c
quantum3theory <ciprian.pantea@yahoo.com>
2023-03-13 22:08:49 +0000

    adds corrections to reflect AiCore's setup

--- milestone_5.py
+++ milestone_5.py
@@ -54,7 +54,7 @@ def play_game(word_list):
     num_lives = 5
     game = Hangman(word_list, num_lives)
     while True:
-        if game.num_lives == 0:
+        if num_lives == 0:
             print('You lost!')
             break
         elif game.num_letters > 0:
